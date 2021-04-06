bc2aab3eaf26656a8d99a5032176eaf4fb7d6cec
MackRoe <elaine.music@students.makeschool.com>
2021-04-06 18:48:10 -0400

    create schema

--- server.js
+++ server.js
@@ -2,3 +2,13 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
+
+// Create a schema
+const schema = buildSchema(`
+type About {
+  message: String!
+}
+
+type Query {
+  getAbout: About
+}`)

15a8059a884d50d040032beb702052d18fec6e4a
MackRoe <elaine.music@students.makeschool.com>
2021-04-06 18:50:01 -0400

    create express app

--- server.js
+++ server.js
@@ -19,3 +19,19 @@ const root = {
     return { message: 'Hello World' }
   }
 }
+
+// Create an express app
+const app = express()
+
+// Define a route for GraphQL
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+// Start this app
+const port = 4000
+app.listen(port, () => {
+  console.log(`Running on port: ${port}`)
+})
